34acef753c7af93666d8df7b1e707d311b40e26d
Kayli Gerka <142949424+kaygerka@users.noreply.github.com>
2025-06-13 18:21:55 -0400

    Update myMalloc.h

--- myMalloc.h
+++ myMalloc.h
@@ -23,49 +23,16 @@
  */
 #define ALLOC_HEADER_SIZE (sizeof(header) - (2 * sizeof(header *)))
 
-/* The minimum size request the allocator will service */
+// The minimum size request the allocator will service 
 #define MIN_ALLOCATION 8
 
-/**
- * @brief enum representing the allocation state of a block
- *
- * enums provice a method of specifying a set of named values
- * http://en.cppreference.com/w/c/language/enum
- */
+// @brief enum representing the allocation state of a block
 enum state {
   UNALLOCATED = 0,
   ALLOCATED = 1,
   FENCEPOST = 2,
 };
 
-/*
- * The header contains all metadata about a block to be allocated
- * The size fields allow accessing the neighboring blocks in memory by
- * calculating their stating and ending addresses.
- *
- * When a block is free the free list pointer fields are set to the next
- * and previous blocks in the free list to allow faster traversal of the
- * freed blocks
- *
- * When a block is allocated the user's data starts by using the 16 bytes
- * reserved for the freelist pointers
- *
- * The zero length array at the end of the struct is the beginning of the
- * usable data in the block.
- *
- * FIELDS ALWAYS PRESENT
- * size_t size The size of the current block *including metadata*
- * size_t left_size The size of the block to the left (in memory)
- *
- * FIELDS PRESENT WHEN FREE
- * header * next The next block in the free list (only valid if free)
- * header * prev The previous block in the free list (only valid if free)
- *
- * FIELD PRESENT WHEN ALLOCATED
- * size_t[] canary magic value to detetmine if a block as been corrupted
- *
- * char[] data first byte of data pointed to by the list
- */
 typedef struct header {
   size_t size_state;
   size_t left_size;
@@ -79,11 +46,6 @@ typedef struct header {
     char data[0];
   };
 } header;
-
-// Helper functions for getting and storing size and state from header
-// Since the size is a multiple of 8, the last 3 bits are always 0s.
-// Therefore we use the 3 lowest bits to store the state of the object.
-// This is going to save 8 bytes in all objects.
 
 static inline size_t get_size(header * h) {
 	return h->size_state & ~0x3;
@@ -109,8 +71,6 @@ static inline void set_size_and_state(header * h, size_t size, enum state s) {
 
 // Malloc interface
 void * my_malloc(size_t size);
-void * my_calloc(size_t nmemb, size_t size);
-void * my_realloc(void * ptr, size_t size);
 void my_free(void * p);
 
 // Debug list verifitcation
@@ -119,42 +79,11 @@ bool verify();
 // Helper to find a block's right neighbor
 header * get_right_header(header * h);
 
-/*
- * Global variables used in malloc that are needed by other C files
- *
- * extern tells the compiler that the variables exist in another file and
- * will be present when the final binary is linked
- */
 extern void * base;
 extern header freelistSentinels[];
 extern char freelist_bitmap[];
 extern header * osChunkList[];
 extern size_t numOsChunks;
 
-/* Define printFormatter to be a function pointer type taking a sinle parameter
- * (a header pointer) and returning void
- *
- * This allows the print freelist and tags functions to take various printing
- * functions depending on the kind of output desired
- *
- * https://www.cprogramming.com/tutorial/function-pointers.html
- */
-typedef void (*printFormatter)(header *);
-
-/* Functions defining a format to print */
-void basic_print(header * block);
-void print_list(header * block);
-void print_object(header * block);
-void print_status(header * block);
-
-/* Functions to print the freelist and boundary tag data structures taking a
- * one of the above printing functions as a function pointer
- */
-void freelist_print(printFormatter pf);
-void tags_print(printFormatter pf);
-
-/* Helpers */
-void print_sublist(printFormatter pf, header * start, header * end);
-void print_pointer(void * p);
 
 #endif // MY_MALLOC_H
