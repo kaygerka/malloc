3d42a0587ddcc8da929b34bc819a6231eefcd008
Kayli Gerka <142949424+kaygerka@users.noreply.github.com>
2025-06-13 18:19:33 -0400

    Update myMalloc.c

--- myMalloc.c
+++ myMalloc.c
@@ -7,74 +7,51 @@
 #include <unistd.h>
 #include <assert.h>
 #include <stdbool.h>
-
 #include <stdint.h>
-
 #include "myMalloc.h"
-
 #define MALLOC_COLOR "MALLOC_DEBUG_COLOR"
-
 
 static bool check_env;
 static bool use_color;
-
 static void remove_from_freelist(header *block);
 static int get_index_for_size(size_t size);
 static void insert_into_freelist(header *freelist, header *block);
 
-
-/*
- * Mutex to ensure thread safety for the freelist
- */
-static pthread_mutex_t mutex;
-
-/*
- * Array of sentinel nodes for the freelists
- */
+phread_mutex_t mutex;
+//Array of sentinel nodes for the freelists
 header freelistSentinels[N_LISTS];
-
 /*
  * Pointer to the second fencepost in the most recently allocated chunk from
  * the OS. Used for coalescing chunks
  */
 header * lastFencePost;
-
-/*
- * Pointer to maintian the base of the heap to allow printing based on the
- * distance from the base of the heap
- */ 
 void * base;
-
-/*
- * List of chunks allocated by  the OS for printing boundary tags
- */
 header * osChunkList [MAX_OS_CHUNKS];
 size_t numOsChunks = 0;
-
 /*
  * direct the compiler to run the init function before running main
  * this allows initialization of required globals
  */
 static void init (void) __attribute__ ((constructor));
 
-// Helper functions for manipulating pointers to headers
+//  HELPER:  for manipulating pointers to headers
 static inline header * get_header_from_offset(void * ptr, ptrdiff_t off);
 static inline header * get_left_header(header * h);
 static inline header * ptr_to_header(void * p);
 
-// Helper functions for allocating more memory from the OS
+//  HELPER:  for allocating more memory from the OS
 static inline void initialize_fencepost(header * fp, size_t left_size);
 static inline void insert_os_chunk(header * hdr);
 static inline void insert_fenceposts(void * raw_mem, size_t size);
 static header * allocate_chunk(size_t size);
 
-// Helper functions for freeing a block
+//  HELPER:  for freeing a block
 static inline void deallocate_object(void * p);
 
-// Helper functions for allocating a block
+//  HELPER:  for allocating a block
 static inline header * allocate_object(size_t raw_size);
 
-// Helper functions for verifying that the data structures are structurally 
+//  HELPER:  for verifying that the data structures are structurally 
 // valid
 static inline header * detect_cycles();
 static inline header * verify_pointers();
@@ -83,75 +60,42 @@ static inline header * verify_chunk(header * chunk);
 static inline bool verify_tags();
 
 static void init();
-
 static bool isMallocInitialized;
 
-/**
- * @brief Helper function to retrieve a header pointer from a pointer and an 
- *        offset
- *
- * @param ptr base pointer
- * @param off number of bytes from base pointer where header is located
- *
- * @return a pointer to a header offset bytes from pointer
- */
+// HELPER: to retrieve a header pointer from a pointer and an offset
 static inline header * get_header_from_offset(void * ptr, ptrdiff_t off) {
 	return (header *)((char *) ptr + off);
 }
 
-/**
- * @brief Helper function to get the header to the right of a given header
- *
- * @param h original header
- *
- * @return header to the right of h
- */
+
+//HELPER: to get the header to the right of a given header
 header * get_right_header(header * h) {
 	return get_header_from_offset(h, get_size(h));
 }
 
-/**
- * @brief Helper function to get the header to the left of a given header
- *
- * @param h original header
- *
- * @return header to the right of h
- */
+
+//HELPER: to get the header to the left of a given header
 inline static header * get_left_header(header * h) {
   return get_header_from_offset(h, -h->left_size);
 }
 
-/**
- * @brief Fenceposts are marked as always allocated and may need to have
- * a left object size to ensure coalescing happens properly
- *
- * @param fp a pointer to the header being used as a fencepost
- * @param left_size the size of the object to the left of the fencepost
- */
+ //HELPER: Fenceposts are marked as always allocated and may need to have  a left object size to ensure coalescing happens properly
 inline static void initialize_fencepost(header * fp, size_t left_size) {
 	set_state(fp,FENCEPOST);
 	set_size(fp, ALLOC_HEADER_SIZE);
 	fp->left_size = left_size;
 }
 
-/**
- * @brief Helper function to maintain list of chunks from the OS for debugging
- *
- * @param hdr the first fencepost in the chunk allocated by the OS
- */
+//HELPER:to maintain list of chunks from the OS for debugging
 inline static void insert_os_chunk(header * hdr) {
   if (numOsChunks < MAX_OS_CHUNKS) {
     osChunkList[numOsChunks++] = hdr;
   }
 }
 
-/**
- * @brief given a chunk of memory insert fenceposts at the left and 
+ /**  HELPER: given a chunk of memory insert fenceposts at the left and 
  * right boundaries of the block to prevent coalescing outside of the
  * block
- *
- * @param raw_mem a void pointer to the memory chunk to initialize
- * @param size the size of the allocated chunk
  */
 inline static void insert_fenceposts(void * raw_mem, size_t size) {
   // Convert to char * before performing operations
@@ -166,15 +110,7 @@ inline static void insert_fenceposts(void * raw_mem, size_t size) {
   initialize_fencepost(rightFencePost, size - 2 * ALLOC_HEADER_SIZE);
 }
 
-/**
- * @brief Allocate another chunk from the OS and prepare to insert it
- * into the free list
- *
- * @param size The size to allocate from the OS
- *
- * @return A pointer to the allocable block in the chunk (just after the 
- * first fencpost)
- */
+ // Allocate another chunk from the OS and prepare to insert it into the free list
 static header * allocate_chunk(size_t size) {
   void * mem = sbrk(size);
   insert_fenceposts(mem, size);
@@ -185,13 +121,8 @@ static header * allocate_chunk(size_t size) {
   return hdr;
 }
 
-/**
- * @brief Helper allocate an object given a raw request size from the user ===========================================
- *
- * @param raw_size number of bytes the user needs
- *
- * @return A block satisfying the user's request
- */
+
+//Helper allocate an object given a raw request size from the user ===========================================
 static inline header * allocate_object(size_t raw_size) {
   // TODO implement allocation  TASK 1 ALLOCATION 
 
@@ -245,7 +176,6 @@ static inline header * allocate_object(size_t raw_size) {
     // if we found a freeObject block break
     if (freeObject) break;
   }
-  // END OF GUSTAVOS CODE  
 
 
 // CASE: NO FREE OBJECT FOUND - we need more memory
@@ -346,27 +276,13 @@ static inline header * allocate_object(size_t raw_size) {
   }
 }
 
-
-
-
-
-
-/**
- * @brief Helper to get the header from a pointer allocated with malloc
- *
- * @param p pointer to the data region of the block
- *
- * @return A pointer to the header of the block
- */
+//Helper to get the header from a pointer allocated with malloc
 static inline header * ptr_to_header(void * p) {
   return (header *)((char *) p - ALLOC_HEADER_SIZE); //sizeof(header));
 }
 
-/**
- * @brief Helper to manage deallocation of a pointer returned by the user
- *
- * @param p The pointer returned to the user by a call to malloc
- */
+
+//Helper to manage deallocation of a pointer returned by the useR 
 static inline void deallocate_object(void * p) { // --------------------------------------------------------------
   // TODO implement deallocation
   // checks if the pointer exists, if NULL returns
@@ -477,23 +393,14 @@ static inline void deallocate_object(void * p) { // ----------------------------
     }
 }
 
-
-
-
-
-
-
-/*
- * HELPER: Removes an object from the free list
- */
+ // HELPER: Removes an object from the free list
  static void remove_from_freelist(header *block) {
     block->prev->next = block->next;
     block->next->prev = block->prev;
 }
 
-/*
- * HELPER: Gets the correct index of the free list to insert the object into it
- */
+
+//HELPER: Gets the correct index of the free list to insert the object into it
 static int get_index_for_size(size_t size) {
   const int SIZE_CLASS = 8; // 8-byte increments
   size_t allocable = size - ALLOC_HEADER_SIZE;
@@ -505,9 +412,8 @@ static int get_index_for_size(size_t size) {
   return (index < N_LISTS - 1) ? index : N_LISTS - 1;
 }
 
-/*
- * HELPER: insert object into free list
- */ 
+
+ //HELPER: insert object into free list
 static void insert_into_freelist(header *freelist, header *block) {
     block->next = freelist->next;
     block->prev = freelist;
@@ -515,33 +421,7 @@ static void insert_into_freelist(header *freelist, header *block) {
     freelist->next = block;
 }
 
-/**
- * @brief Helper to detect cycles in the free list
- * https://en.wikipedia.org/wiki/Cycle_detection#Floyd's_Tortoise_and_Hare
- *
- * @return One of the nodes in the cycle or NULL if no cycle is present
- */
-static inline header * detect_cycles() {
-  for (int i = 0; i < N_LISTS; i++) {
-    header * freelist = &freelistSentinels[i];
-    for (header * slow = freelist->next, * fast = freelist->next->next; 
-         fast != freelist; 
-         slow = slow->next, fast = fast->next->next) {
-      if (slow == fast) {
-        return slow;
-      }
-    }
-  }
-  return NULL;
-}
-
-/**
- * @brief Helper to verify that there are no unlinked previous or next pointers
- *        in the free list
- *
- * @return A node whose previous and next pointers are incorrect or NULL if no
- *         such node exists
- */
+// HELPER: to verify that there are no unlinked previous or next pointers in the free list
 static inline header * verify_pointers() {
   for (int i = 0; i < N_LISTS; i++) {
     header * freelist = &freelistSentinels[i];
@@ -554,12 +434,7 @@ static inline header * verify_pointers() {
   return NULL;
 }
 
-/**
- * @brief Verify the structure of the free list is correct by checkin for 
- *        cycles and misdirected pointers
- *
- * @return true if the list is valid
- */
+// HELPER: Verify the structure of the free list is correct by checking for cycles and misdirected pointers
 static inline bool verify_freelist() {
   header * cycle = detect_cycles();
   if (cycle != NULL) {
@@ -578,14 +453,7 @@ static inline bool verify_freelist() {
   return true;
 }
 
-/**
- * @brief Helper to verify that the sizes in a chunk from the OS are correct
- *        and that allocated node's canary values are correct
- *
- * @param chunk AREA_SIZE chunk allocated from the OS
- *
- * @return a pointer to an invalid header or NULL if all header's are valid
- */
+// Helper to verify that the sizes in a chunk from the OS are correct and that allocated node'svalues are correct 
 static inline header * verify_chunk(header * chunk) {
 	if (get_state(chunk) != FENCEPOST) {
 		fprintf(stderr, "Invalid fencepost\n");
@@ -605,10 +473,8 @@ static inline header * verify_chunk(header * chunk) {
 }
 
 /**
- * @brief For each chunk allocated by the OS verify that the boundary tags
+ *   For each chunk allocated by the OS verify that the boundary tags
  *        are consistent
- *
- * @return true if the boundary tags are valid
  */
 static inline bool verify_tags() {
   for (size_t i = 0; i < numOsChunks; i++) {
@@ -622,21 +488,17 @@ static inline bool verify_tags() {
 }
 
 /**
- * @brief Initialize mutex lock and prepare an initial chunk of memory for allocation
+ *   Initialize mutex lock and prepare an initial chunk of memory for allocation
  */
 static void init() {
   // Initialize mutex for thread safety
   pthread_mutex_init(&mutex, NULL);
-
-
 
   #ifdef DEBUG
     // Manually set printf buffer so it won't call malloc when debugging the allocator
     setvbuf(stdout, NULL, _IONBF, 0);
   #endif // DEBUG
   
-
-
   // Allocate the first chunk from the OS
   header * block = allocate_chunk(ARENA_SIZE);
 
@@ -673,102 +535,10 @@ void * my_malloc(size_t size) {
   pthread_mutex_unlock(&mutex);
   return hdr;
 }
-
-void * my_calloc(size_t nmemb, size_t size) {
-  return memset(my_malloc(size * nmemb), 0, size * nmemb);
-}
-
-void * my_realloc(void * ptr, size_t size) {
-  void * mem = my_malloc(size);
-  memcpy(mem, ptr, size);
-  my_free(ptr);
-  return mem; 
-}
-
 void my_free(void * p) {
   pthread_mutex_lock(&mutex);
   deallocate_object(p);
   pthread_mutex_unlock(&mutex);
-}
-
-bool verify() {
-  return verify_freelist() && verify_tags();
-}
-
-/**
- * @brief Print just the block's size
- *
- * @param block The block to print
- */
-void basic_print(header * block) {
-	printf("[%zd] -> ", get_size(block));
-}
-
-/**
- * @brief Print just the block's size
- *
- * @param block The block to print
- */
-void print_list(header * block) {
-	printf("[%zd]\n", get_size(block));
-}
-
-/**
- * @brief return a string representing the allocation status
- *
- * @param allocated The allocation status field
- *
- * @return A string representing the allocation status
- */
-static inline const char * allocated_to_string(char allocated) {
-  switch(allocated) {
-    case UNALLOCATED: 
-      return "false";
-    case ALLOCATED:
-      return "true";
-    case FENCEPOST:
-      return "fencepost";
-  }
-  assert(false);
-}
-
-static bool check_color() {
-  if (!check_env) {
-    // genenv allows accessing environment varibles
-    const char * var = getenv(MALLOC_COLOR);
-    use_color = var != NULL && !strcmp(var, "1337_CoLoRs");
-    check_env = true;
-  }
-  return use_color;
-}
-
-/**
- * @brief Change the tty color based on the block's allocation status
- *
- * @param block The block to print the allocation status of
- */
-static void print_color(header * block) {
-  if (!check_color()) {
-    return;
-  }
-
-  switch(get_state(block)) {
-    case UNALLOCATED:
-      printf("\033[0;32m");
-      break;
-    case ALLOCATED:
-      printf("\033[0;34m");
-      break;
-    case FENCEPOST:
-      printf("\033[0;33m");
-      break;
-  }
-}
-
-static void clear_color() {
-  if (check_color()) {
-    printf("\033[0;0m");
-  }
 }
 
 static inline bool is_sentinel(void * p) {
@@ -778,148 +548,4 @@ static inline bool is_sentinel(void * p) {
     }
   }
   return false;
-}
-
-/**
- * @brief Print the free list pointers if RELATIVE_POINTERS is set to true
- * then print the pointers as an offset from the base of the heap. This allows
- * for determinism in testing. 
- * (due to ASLR https://en.wikipedia.org/wiki/Address_space_layout_randomization#Linux)
- *
- * @param p The pointer to print
- */
-void print_pointer(void * p) {
-  if (is_sentinel(p)) {
-    printf("SENTINEL");
-  } else {
-    if (RELATIVE_POINTERS) {
-      printf("%04zd", p - base);
-    } else {
-      printf("%p", p);
-    }
-  }
-}
-
-/**
- * @brief Verbose printing of all of the metadata fields of each block
- *
- * @param block The block to print
- */
-void print_object(header * block) {
-  print_color(block);
-
-  printf("[\n");
-  printf("\taddr: ");
-  print_pointer(block);
-  puts("");
-  printf("\tsize: %zd\n", get_size(block) );
-  printf("\tleft_size: %zd\n", block->left_size);
-  printf("\tallocated: %s\n", allocated_to_string(get_state(block)));
-  if (!get_state(block)) {
-    printf("\tprev: ");
-    print_pointer(block->prev);
-    puts("");
-
-    printf("\tnext: ");
-    print_pointer(block->next);
-    puts("");
-  }
-  printf("]\n");
-
-  clear_color();
-}
-
-/**
- * @brief Simple printer that just prints the allocation status of each block
- *
- * @param block The block to print
- */
-void print_status(header * block) {
-  print_color(block);
-  switch(get_state(block)) {
-    case UNALLOCATED:
-      printf("[U]");
-      break;
-    case ALLOCATED:
-      printf("[A]");
-      break;
-    case FENCEPOST:
-      printf("[F]");
-      break;
-  }
-  clear_color();
-}
-
-/*
-static void print_bitmap() {
-  printf("bitmap: [");
-  for(int i = 0; i < N_LISTS; i++) {
-    if ((freelist_bitmap[i >> 3] >> (i & 7)) & 1) {
-      printf("\033[32m#\033[0m");
-    } else {
-      printf("\033[34m_\033[0m");
-    }
-    if (i % 8 == 7) {
-      printf(" ");
-    }
-  }
-  puts("]");
-}
-*/
-
-/**
- * @brief Print a linked list between two nodes using a provided print function
- *
- * @param pf Function to perform the actual printing
- * @param start Node to start printing at
- * @param end Node to stop printing at
- */
-void print_sublist(printFormatter pf, header * start, header * end) {  
-  for (header * cur = start; cur != end; cur = cur->next) {
-    pf(cur); 
-  }
-}
-
-/**
- * @brief print the full freelist
- *
- * @param pf Function to perform the header printing
- */
-void freelist_print(printFormatter pf) {
-  if (!pf) {
-    return;
-  }
-
-  for (size_t i = 0; i < N_LISTS; i++) {
-    header * freelist = &freelistSentinels[i];
-    if (freelist->next != freelist) {
-      printf("L%zu: ", i);
-      print_sublist(pf, freelist->next, freelist);
-      puts("");
-    }
-    fflush(stdout);
-  }
-}
-
-/**
- * @brief print the boundary tags from each chunk from the OS
- *
- * @param pf Function to perform the header printing
- */
-void tags_print(printFormatter pf) {
-  if (!pf) {
-    return;
-  }
-
-  for (size_t i = 0; i < numOsChunks; i++) {
-    header * chunk = osChunkList[i];
-    pf(chunk);
-    for (chunk = get_right_header(chunk);
-         get_state(chunk) != FENCEPOST; 
-         chunk = get_right_header(chunk)) {
-        pf(chunk);
-    }
-    pf(chunk);
-    fflush(stdout);
-  }
 }
